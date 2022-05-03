d34e2271d6786a5a7725b73124ad34417886d9fa
EC2 Default User <ec2-user@ip-172-31-46-237.ec2.internal>
2022-05-03 05:19:04 +0000

    Preparation of necessary files

---
+++ db/migrate/20220503050717_create_books.rb
@@ -0,0 +1,11 @@
+class CreateBooks < ActiveRecord::Migration[6.1]
+  def change
+    create_table :books do |t|
+      t.string :title, null: false
+      t.string :body, null: false
+      t.string :user_id, null: false
+
+      t.timestamps
+    end
+  end
+end
