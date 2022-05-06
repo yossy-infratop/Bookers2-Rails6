6520299437998a86e1714360afc41a7f77cc5aa3
EC2 Default User <ec2-user@ip-172-31-46-237.ec2.internal>
2022-05-06 08:04:53 +0000

    remote

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  #
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
