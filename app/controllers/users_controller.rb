eae3d75edabefd99fc8e3b53e79ecde30a5de8df
yossy <yoshikawa.akito.infratop@gmail.com>
2022-06-15 15:54:51 +0000

    8b

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,12 @@ class UsersController < ApplicationController
 
   def show
     @books = @user.books
+    @book_count = [@books.where(created_at: Time.zone.now.all_day).count]
+    @days = ["今日"]
+    for num in 1..6 do
+      @book_count.push(@books.where(created_at: num.day.ago.all_day).count)
+      @days.push("#{num}日前")
+    end
     @new_book = Book.new
   end
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,8 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
 import '@fortawesome/fontawesome-free/js/all'
+import Chart from "chart.js/auto";
+global.Chart = Chart;
 
 Rails.start()
 Turbolinks.start()
